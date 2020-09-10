56a2f4597a3ba1809a0d5b2da0733af942b2e12c
Scott Million <scottmillion@gmail.com>
2020-09-10 15:07:20 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+app = express();
+
+
+
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.listen(process.env.PORT, process.env.IP, (req, res) => {
+  console.log("live, sir!")
+});
